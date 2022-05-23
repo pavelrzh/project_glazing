8f5bf7b6ec4f64e7e600e4c1e99515faa335c264
userName <>
2022-05-23 13:21:09 +0500

    modify module

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -101,7 +101,7 @@ const modals = (state) => {
     bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     
-    // showModalByTime('.popup', 60000);
+    showModalByTime('.popup', 10000);
 
 
 };
