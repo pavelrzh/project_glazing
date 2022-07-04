1f7519493520f019adb18d50b54f7f46866f8b5e
userName <>
2022-07-04 16:46:55 +0500

    change dist/

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+const dist = "./"
+// const dist = "./dist/";
 // const dist = "C:/Program Files/OpenServer/domains/test";
 
 gulp.task("copy-html", () => {
